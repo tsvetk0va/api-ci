64b3dc28b5befedce2e41e8d5a3914880d690132
tsvetk0va <kknn2821@mail.ru>
2022-08-04 16:57:18 +0300

    changed test on RUR

---
+++ src/test/java/ru/netology/rest/MobileBankApiMyTest.java
@@ -0,0 +1,32 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiMyTest {
+
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[0].currency", equalTo("RUR"))
+        ;
+    }
+}
+
+
+
